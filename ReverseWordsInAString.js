284fca26fa2dd38b5b85c807f9b695f3dc9986e3
myeong-jae-hwi <audwognl@gmail.com>
2025-04-29 16:37:09 +0900

    Problem: 단어별 문자 뒤집기

--- ReverseWordsInAString.js
+++ ReverseWordsInAString.js
@@ -1,3 +1,13 @@
+/**
+ * Problem:
+ * Given a string s, reverse the order of words.
+ * Words are separated by spaces.
+ *
+ * Example:
+ * Input: "the sky is blue"
+ * Output: "blue is sky the"
+ */
+
 function ReverseWords(str) {
   return str.split(" ").reverse().join(" ");
 }
